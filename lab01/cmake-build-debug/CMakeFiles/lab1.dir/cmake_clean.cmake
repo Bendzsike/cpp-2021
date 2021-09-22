6b92bdd4d01c6a692ded30203804ac075506d7af
Bendzsike <bodor.benjamin@outlook.com>
2021-09-22 08:22:13 +0300

    Initial commit

---
+++ lab01/cmake-build-debug/CMakeFiles/lab1.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab1.dir/C_/Users/Benjamin/Desktop/Egyetem/II._Ev/I._Felev/CPP/Labor/lab02/main.cpp.obj"
+  "CMakeFiles/lab1.dir/main.cpp.obj"
+  "lab1.exe"
+  "lab1.exe.manifest"
+  "lab1.pdb"
+  "liblab1.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lab02/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello lab02!" << endl;
+    return 0;
+}
