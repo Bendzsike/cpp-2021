bc1b20abf737ea3491e30cd01765c1c81460d405
Bendzsike <bodor.benjamin@outlook.com>
2021-11-03 10:23:25 +0200

    Finished lab07 extra

--- lab07extra/NodeBST.cpp
+++ lab07extra/NodeBST.cpp
@@ -15,15 +15,38 @@ void NodeBST::inorder(Node *node) const {
 }
 
 void NodeBST::preorder(Node *node) const {
+    if(node == nullptr) {
+        return;
+    }
 
+    cout << node->value << " ";
+
+    preorder(node->left);
+
+    preorder(node->right);
 }
 
 void NodeBST::postorder(Node *node) const {
+    if(node == nullptr) {
+        return;
+    }
 
+    postorder(node->left);
+
+    postorder(node->right);
+
+    cout << node->value << " ";
 }
 
 void NodeBST::deleteSubTree(Node *node) {
-
+    if(node->left) {
+        deleteSubTree(node->left);
+    }
+    if(node->right) {
+        deleteSubTree(node->right);
+    }
+    delete node;
+    node = nullptr;
 }
 
 NodeBST::NodeBST() {
@@ -31,7 +54,7 @@ NodeBST::NodeBST() {
 }
 
 NodeBST::~NodeBST() {
-
+    deleteSubTree(root);
 }
 
 void NodeBST::insert(int val) {
@@ -54,13 +77,29 @@ void NodeBST::insert(int val) {
             }
             temp = temp->right;
         } else {
+            //Value already exists
             break;
         }
     }
 }
 
 bool NodeBST::find(int val) const {
+    if(root == nullptr) {
+        cout << "Error, tree doesn't exist!" << endl;
+        return false;
+    }
+    Node *temp = root;
+    while(temp) {
+        if(val < temp->value) {
+            temp = temp->left;
+        } else if(val > temp->value){
 
+            temp = temp->right;
+        } else {
+            return true;
+        }
+    }
+    return false;
 }
 
 void NodeBST::print_inorder() const {
@@ -69,9 +108,11 @@ void NodeBST::print_inorder() const {
 }
 
 void NodeBST::print_preorder() const {
-
+    preorder(root);
+    cout << endl;
 }
 
 void NodeBST::print_postorder() const {
-
+    postorder(root);
+    cout << endl;
 }
